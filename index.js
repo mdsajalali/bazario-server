efcd9e4abf640fe68d25797e9475839679d5f129
mdsajalali <mdsajalali2020@gmail.com>
2025-07-07 20:39:22 +0600

    added: vercel file

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ connectDB();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API Working");
