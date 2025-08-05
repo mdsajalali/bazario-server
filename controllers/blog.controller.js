a53cc3efda20a088092dbb6a9eff9fd5172f6de2
mdsajalali <mdsajalali2020@gmail.com>
2025-08-05 15:50:42 +0600

    added: blog update functionality

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -39,8 +39,26 @@ const getSingleBlog = async (req, res) => {
   }
 };
 
+const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedBlog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+    res.status(200).json({ message: "Blog updated", blog: updatedBlog });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to update blog", message: err.message });
+  }
+};
+
 module.exports = {
   createBlog,
   getAllBlogs,
   getSingleBlog,
+  updateBlog,
 };
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,11 +3,13 @@ const {
   createBlog,
   getAllBlogs,
   getSingleBlog,
+  updateBlog,
 } = require("../controllers/blog.controller");
 const router = express.Router();
 
 router.get("/", getAllBlogs);
 router.get("/:id", getSingleBlog);
 router.post("/", createBlog);
+router.put("/:id", updateBlog);
 
 module.exports = router;
